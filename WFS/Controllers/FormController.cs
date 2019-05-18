849b1e613e6207748f096875faac2a62f9856781
King <32458909@qq.com>
2019-05-18 23:22:16 +0800

    2

--- WFS/Controllers/FormController.cs
+++ WFS/Controllers/FormController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using WFS.Helpers;
 using WFS.Models;
 
 namespace WFS.Controllers
@@ -15,11 +17,63 @@ namespace WFS.Controllers
             FormPrintModel model;
             using (WFSContext db = new WFSContext())
             {
+                var form = db.Forms
+                    .Include("ProcessLog")
+                    .Where(x => x.ID.Trim() == id.Trim()).FirstOrDefault();
+                if(form == null)
+                {
+                    return View();
+                }
+                model = (from f in db.Forms
+                             join u in db.Users on f.CreateBy.Trim() equals u.ID.Trim()
+                             select new FormPrintModel()
+                             {
+                                 ID = f.ID,
+                                 Content = f.Content,
+                                 Cost = f.Cost,
+                                 CostChinese = "",
+                                 Date = f.CreateTime,
+                                 DeptName = u.Dept.Name.Trim(),
+                                 //P1 = f.ProcessLog.Where(x=>x.ProcessCode == ProcessCode.L0)
+                                 Title = f.Title,
+                                 Type = f.Type == FormType.Budget ? "" : "",
+                                 User = u.Name.Trim()
+                             }).FirstOrDefault();
+                var p1 = form.ProcessLog.FirstOrDefault(x => x.ProcessCode == ProcessCode.L10);
+                if(p1 != null)
+                {
+                    model.P1 = string.Format("{0}<br/>{1}<br/>{2}", p1.CreateBy, p1.CreateDate.ToString("yyyy/MM/dd"), FormStrategy.StatusString(p1.status));
+                }
 
+                var p2 = form.ProcessLog.FirstOrDefault(x => x.ProcessCode == ProcessCode.L20);
+                if (p2 != null)
+                {
+                    model.P2 = string.Format("{0}<br/>{1}<br/>{2}", p2.CreateBy, p2.CreateDate.ToString("yyyy/MM/dd"), FormStrategy.StatusString(p2.status));
+                }
+
+                var p3 = form.ProcessLog.FirstOrDefault(x => x.ProcessCode == ProcessCode.L30);
+                if (p3 != null)
+                {
+                    model.P3 = string.Format("{0}<br/>{1}<br/>{2}", p3.CreateBy, p3.CreateDate.ToString("yyyy/MM/dd"), FormStrategy.StatusString(p3.status));
+                }
+
+                var p4 = form.ProcessLog.FirstOrDefault(x => x.ProcessCode == ProcessCode.L40);
+                if (p4 != null)
+                {
+                    model.P4 = string.Format("{0}<br/>{1}<br/>{2}", p4.CreateBy, p4.CreateDate.ToString("yyyy/MM/dd"), FormStrategy.StatusString(p4.status));
+                }
+
+                model.CostChinese = ConvertToChinese(model.Cost);
             }
-            return View();
+            return View(model);
+        }
+
+        public static String ConvertToChinese(Decimal number)
+        {
+            var s = number.ToString("#L#E#D#C#K#E#D#C#J#E#D#C#I#E#D#C#H#E#D#C#G#E#D#C#F#E#D#C#.0B0A");
+            var d = Regex.Replace(s, @"((?<=-|^)[^1-9]*)|((?'z'0)[0A-E]*((?=[1-9])|(?'-z'(?=[F-L\.]|$))))|((?'b'[F-L])(?'z'0)[0A-L]*((?=[1-9])|(?'-z'(?=[\.]|$))))", "${b}${z}");
+            var r = Regex.Replace(d, ".", m => "负元空零壹贰叁肆伍陆柒捌玖空空空空空空空分角拾佰仟万亿兆京垓秭穰"[m.Value[0] - '-'].ToString());
+            return r;
         }
     }
-
-    
 }
